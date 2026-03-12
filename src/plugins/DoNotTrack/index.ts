c1a228981a8e179840b430bfedcd97612da7411a
Snues <81967520+Snusene@users.noreply.github.com>
2026-03-12 21:39:09 +0100

    Update for Sentry v8.55.0

--- src/plugins/DoNotTrack/index.ts
+++ src/plugins/DoNotTrack/index.ts
@@ -17,11 +17,11 @@ export default class DoNotTrack extends Plugin {
     }
 
     onStart() {
-        
+
         if (Analytics) {
             Patcher.instead(this.meta.name, Analytics.default, "track", () => {});
         }
-        
+
         if (NativeModule) {
             Patcher.instead(this.meta.name, NativeModule, "ensureModule", (_, [moduleName]: [string], originalFunction) => {
                 if (moduleName?.includes("discord_rpc")) return;
@@ -29,26 +29,15 @@ export default class DoNotTrack extends Plugin {
             });
         }
 
-        // No more global processors
-        window?.__SENTRY__?.globalEventProcessors?.splice(0, window?.__SENTRY__?.globalEventProcessors?.length);
-
-        // Kill sentry logs if it still exists here
-        window?.__SENTRY__?.logger?.disable();
-
-        const SentryHub = window.DiscordSentry?.getCurrentHub?.();
-        if (SentryHub) {
-            SentryHub.getClient()?.close?.(0); // Kill reporting
-
-            const scope = SentryHub.getScope();
-            scope?.clear?.(); // Delete PII
-            scope?.setFingerprint?.(null); // Remove fingerprinting
-            
-            SentryHub?.setUser(null); // Unset default PII
-            SentryHub?.setTags({}); // Empty tracking tags
-            SentryHub?.setExtras({}); // Empty extra idendtifiable data
-
-            SentryHub?.endSession(); // End current session
+        const client = window.DiscordSentry?.getClient?.();
+        if (client) {
+            client.getOptions().enabled = false;
+            client.getOptions().dsn = '';
+            client.close(0);
         }
+        window.DiscordSentry?.getCurrentScope?.()?.clear?.();
+        window.DiscordSentry?.getIsolationScope?.()?.clear?.();
+        window.DiscordSentry?.getGlobalScope?.()?.clear?.();
 
         for (const method in console) {
             // eslint-disable-next-line no-console
@@ -60,7 +49,7 @@ export default class DoNotTrack extends Plugin {
 
         if (this.settings.stopProcessMonitor) this.disableProcessMonitor();
     }
-    
+
     onStop() {
         Patcher.unpatchAll(this.meta.name);
     }
